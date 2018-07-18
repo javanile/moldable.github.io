d19b77f5503ee15f877bd01a65e2597e931d3b05
Francesco Bianco <info.francescobianco@gmail.com>
2018-07-18 18:06:20 +0200

    Version 0.0.12

---
+++ src/Pages/About.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Javanile\MoldableDocs\Pages;
+
+use Javanile\Elegy\Page;
+use Javanile\MoldableDocs\Samples\People;
+
+class About extends Page
+{
+    public function content()
+    {
+        $this->renderClass('Javanile\\MoldableDocs\\Samples\\People');
+
+        // @block : start 'insert'
+        // Insert one record
+        //People::insert([
+        //    'name' => 'ciao'
+        //]);
+        // @block : end
+
+        $this->renderFileBlock(__FILE__, 'insert');
+
+        //People::desc();
+        //People::dump();
+    }
+}
