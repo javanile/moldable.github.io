ed04a2a9b2e235357504bd084e6eaf95192aa869
Francesco Bianco <info.francescobianco@gmail.com>
2018-08-01 02:06:13 +0200

    Version 0.0.14

--- templates/header.php
+++ templates/header.php
@@ -6,15 +6,8 @@
         <meta name="author" content="CJ Patoilo">
         <meta name="description" content="Milligram provides a minimal setup of styles for a fast and clean starting point. Specially designed for better performance and higher productivity with fewer properties to reset resulting in cleaner code.">
         <title>Milligram | A minimalist CSS framework.</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.13.0/themes/prism.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.13.0/prism.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.13.0/plugins/normalize-whitespace/prism-normalize-whitespace.js"></script>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/solid.css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/fontawesome.css">
-        <link rel="stylesheet" href="https://unpkg.com/mustard-ui@latest/dist/css/mustard-ui.min.css">
-        <link rel="stylesheet" href="https://cdn.rawgit.com/the-teacher/protozaur/master/ptz/framework.min.css">
+        <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
 
